76a25c2ec9c487b27d273c6eb51a69ef692abcc3
Anderson Adolfo <anderson.adolfo1998@gmail.com>
2022-06-24 03:13:26 -0300

    feat: integration faunadb in project

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -1,5 +1,10 @@
+import { query as q } from 'faunadb';
+
 import NextAuth from "next-auth"
 import GithubProvider from "next-auth/providers/github"
+
+import { fauna } from '../../../services/fauna';
+
 export default NextAuth({
 
     providers: [
@@ -13,4 +18,38 @@ export default NextAuth({
             }
         }),
     ],
+    callbacks: {
+        async signIn({ user, account, profile }) {
+
+
+            try {
+                await fauna.query(
+                    q.If(
+                        q.Not(
+                            q.Exists(
+                                q.Match(
+                                    q.Index("user_by_email"),
+                                    // esse email...
+                                    q.Casefold(user.email!)
+                                )
+                            )
+                        ),
+                        // create a user with this email
+                        q.Create(q.Collection("users"), {
+                            data: {
+                                email: user.email
+                            }
+                        }),
+                        // otherwise, search the user by email
+                        q.Get(q.Match(q.Index("user_by_email"), q.Casefold(user.email!)))
+                    )
+                );
+
+                return true;
+            } catch {
+                return false;
+            }
+        },
+    }
+
 })
