592d7248927686e20be877a4ae92b81c5125c7cb
Anderson Adolfo <anderson.adolfo1998@gmail.com>
2022-06-24 10:58:12 -0300

    feat: module by payments of the stripe

--- pages/api/subscribe.ts
+++ pages/api/subscribe.ts
@@ -1,7 +1,19 @@
 /* eslint-disable import/no-anonymous-default-export */
 import { NextApiRequest, NextApiResponse } from "next";
 import { getSession } from "next-auth/react";
+import { fauna } from "../../services/fauna";
 import { stripe } from "../../services/stripe";
+import { query as q } from 'faunadb';
+
+
+type User = {
+    ref: {
+        id: string
+    },
+    data: {
+        stripe_customer_id: string;
+    }
+}
 
 export default async (request: NextApiRequest, response: NextApiResponse) => {
 
@@ -9,16 +21,42 @@ export default async (request: NextApiRequest, response: NextApiResponse) => {
 
         const session = await getSession({ req: request });
 
-        const stripeCustomer = await stripe.customers.create({
-            email: session!.user?.email!,
-        })
+        const user = await fauna.query<User>(
+            q.Get(
+                q.Match(
+                    q.Index('user_by_email'),
+                    q.Casefold(session?.user?.email!)
+                )
+            )
+        );
+
+        let customerId = user.data.stripe_customer_id;
+
+        if (!customerId) {
+            const stripeCustomer = await stripe.customers.create({
+                email: session!.user?.email!,
+            });
+
+            await fauna.query(
+                q.Update(
+                    q.Ref(q.Collection('users'), user.ref.id),
+                    {
+                        data: {
+                            stripe_customer_id: stripeCustomer.id
+                        }
+                    }
+                )
+            )
+
+            customerId = stripeCustomer.id;
+        }
 
         const stripeCheckoutSession = await stripe.checkout.sessions.create({
-            customer: stripeCustomer.id,
+            customer: customerId,
             payment_method_types: ['card'],
             billing_address_collection: 'required',
             line_items: [
-                { price: 'price_1LDuvAAznLmLPXw0x91RaDgg' }
+                { price: 'price_1LDuvAAznLmLPXw0x91RaDgg', quantity: 1 },
             ],
             mode: 'subscription',
             allow_promotion_codes: true,
